5b093414913eba60ac5a7229a9b35c7cb950b954
Shibe Drill <53824200+shibedrill@users.noreply.github.com>
2022-04-05 13:13:00 -0400

    Add files via upload

---
+++ kali-chroot.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Check if running as root and exit if not
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Welcome message
+echo "Kali Linux Chroot Generator"
+echo "by shibedrill"
+echo "This script will automatically configure a Chroot environment in which to run many Kali Linux tools."
+echo "Now please notice: Wireless tools will not work out of the box. However, most other tools will."
+echo "Metasploit, password tools, GUI tools like Burp and Wireshark, and net tools like nmap will work."
+echo ""
+echo "Prerequesites: APT, a stable internet connection"
+echo "Warning: This might take up a lot of space! (>3gb)"
+echo "Do you wish to continue? (y/n)"
+read CONSENT
+if [ CONSENT = "n" ]
+  then exit
+echo ""
+
+# TO-DO: Make this script run on any distro by using whichever package manager is installed
+echo "Step one: Install debootstrap and schroot if not installed."
+apt update && apt install debootstrap schroot wget sed -y
+# TO-DO: Add error handling
+echo "debootstrap and schroot installed successfully!"
+echo ""
+
+echo "Step two: Generate chroot directory"
+echo "Which directory do you want your chroot to be installed in?"
+echo "Default: /srv/chroot/kali"
+# Read chroot path
+read CHROOTPATH
+if [ CHROOTPATH = "" ]
+  then CHROOTPATH=/srv/chroot/kali
+# Make directory if it doesn't exist
+[ -d $CHROOTPATH ] || mkdir $CHROOTPATH
+echo "Directory created successfully!"
+echo ""
+
+# Begin bootstrapping
+echo "Step three: Actually perform the bootstrap"
+echo "Which architecture do you want to use?"
+echo "Options: amd64, i386. Default: amd64"
+# Read chroot architecture
+read CHROOTARCH
+if [ CHROOTARCH = "" ]
+  then CHROOTARCH=amd64
+echo "Beginning bootstrap. This might take a WHILE!"
+debootstrap --variant=buildd --include=kali-linux-core,zsh --arch=$CHROOTARCH kali-rolling $CHROOTPATH http://http.kali.org/kali
+echo "Bootstrap finished!"
+echo ""
+
+# Debug step
+echo "Step four: Debugging and configuring our new installation"
+echo "Adding non-free and contrib sources to sources.list..."
+echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" > $CHROOTPATH/etc/apt/sources.list
+echo "Done"
+echo "Downloading Kali repo pubkey..."
+wget https://archive.kali.org/archive-key.asc -O $CHROOTPATH/etc/apt/trusted.gpg.d/kali-archive-key.asc
+echo "Done"
+echo "Removing statoverride group Debian-exim..."
+echo "Warning! This is a very hacky patch, and it might not work."
+cp $CHROOTPATH/var/lib/dpkg/statoverride $CHROOTPATH/var/lib/dpkg/statoverride.bak
+sed -i '1d' $CHROOTPATH/var/lib/dpkg/statoverride
+echo "Done"
+echo "Setting the chroot to use your display..."
+echo "export DISPLAY=:0.0" >> $CHROOTPATH/root/.zshrc
+echo "Done"
+echo "Setting aliases for host to use the chroot..."
+echo "Set the following alias in your profile or aliases file:"
+echo "alias kali='xhost + && schroot -c kali -u root -d /root && schroot -e --all-sessions && xhost -'"
+echo ""
+
+# Finished!
+echo "Once you configure your aliases, use the command 'kali' to enter your chroot. Enjoy!"
